4686980574db64cf17062f5c5cdfd2c4fcfe1c5c
= <vbarilla@gmail.com>
2016-03-23 16:37:34 -0400

    finalizing mood

--- tweetAnalyzer.js
+++ tweetAnalyzer.js
@@ -43,9 +43,8 @@ var util = require('util');
 
 				tweetWords.forEach(function(word){
 
-					// Make sure the word isn't a hashtag, username, hyperlink, or newline character.
-					if (word[0] != '#' && word[0] != '@' && word.indexOf('http') == -1 
-				    	&& word.indexOf('#') == -1 && word.indexOf('@') == -1 && word != "\n") {
+					// Make sure the word isn't a username, hyperlink, or newline character.
+					if (word[0] != '@' && word.indexOf('http') == -1 && word.indexOf('@') == -1 && word != "\n") {
 						
 						// The word bank presumes lower case and unpunctuated words. If it doesn't already 
 						// satisfy these conditions, then the word must be normalized.
@@ -55,14 +54,50 @@ var util = require('util');
 
 						// After normalization, the word can be checked against the database.
 						if (word != null && wordBank[word] != undefined){
+							console.log(word);
 							scoreWord(word);
 						}
 
 					}	
 				})
 				console.log(multiplier);
+				console.log(mood);
+ 						
+ 				function normalizeWord(word){
+ 					scoreCasing(word);
+ 					scorePunctuation(word)
+ 					word = word.match(/[a-z]+/gi);
+ 					if (word != null && wordBank[word[0].toLowerCase()] != undefined){
+ 						scoreWord(word[0].toLowerCase());
+ 					} else {
+ 						word = null;
+ 					}
 
+ 					// Each all-cap word (or with 2 letters of being all caps) augment the multiplier.
+ 					function scoreCasing(word){
+ 						var caps = word.match(/[A-Z]/g);
+ 						if (caps != null && word.length > 2 && caps.length >= word.split('').length - 2 ){
+ 							multiplier += .2;
+ 						}
+ 					}
 
+ 					// Each '!' augments the multiplier; each basic smiley face augments a mood.
+ 					function scorePunctuation(word){
+ 						var exclam = word.match(/!/g);
+ 						if (exclam != null){
+ 							multiplier += exclam.length * .2;
+ 						}
+
+ 						var happyFace = word.match(/[:)]/g);
+ 						if (happyFace != null){
+ 							mood[2] += 35 * happyFace.length;
+ 						}
+ 						var sadFace = word.match(/[:(]/g);
+ 						if (sadFace != null){
+ 							mood[0] += 35 * sadFace.length;
+ 						}
+ 					}
+ 				}
 
 				// Arbitrarily used a tweet I thought equated to a [160, 0, 0] RGB score, then broke it down 
 				// by the multipliers I (also arbitrarily) assigned. It came out to roughly 10 pixels * a word's score 
@@ -72,13 +107,13 @@ var util = require('util');
 						mood[2] += wordBank[word] * 10;
 					}
 					if (wordBank[word] < 0){
-						mood[0] += wordBank[word] * 10;
+						mood[0] += wordBank[word] * -10;
 					}	
 				}
 			}
 		}
 	}
-	
+
 
 
 
